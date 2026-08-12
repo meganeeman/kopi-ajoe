989bd291591c594c7e8a54da09503bd8a7b50aec
Mr. Ethical <akbarcrushed@gmail.com>
2026-08-12 21:00:24 +0700

    Update logika order

--- src/screen/OrderScreen.js
+++ src/screen/OrderScreen.js
@@ -10,7 +10,7 @@ import {
     TouchableOpacity,
     View,
 } from 'react-native';
-import { supabase } from '../config/supabase';
+import { supabase } from '../../config/supabase';
 import { COLORS } from '../constants/theme';
 
 export default function OrderScreen() {
